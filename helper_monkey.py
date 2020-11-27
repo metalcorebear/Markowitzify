78ea136a44f2fb6ba6ac0891951d77b4faa9d232
metalcorebear <43913302+metalcorebear@users.noreply.github.com>
2020-11-27 18:54:41 -0500

    Update

--- helper_monkey.py
+++ helper_monkey.py
@@ -34,15 +34,75 @@ import statsmodels.api as sml
 def help_():
     helptext = '''Hey, Listen!
     
-    USAGE NOTES:
+USAGE NOTES:
         
-        text 1
-        
-        
-        text 2
-        
-        
-        text 3'''
+## Usage
+
+### Object Instantiation
+`import markowitzify`<br>
+
+`portfolio_object = markowitzify.portfolio(**options)`<br><br>
+
+Attributes:<br>
+* `portfolio_object.portfolio` = Portfolio (Pandas DataFrame).
+* `portfolio_object.cov` = Portfolio covariance matrix (Numpy array).
+* `portfolio_object.optimal` = Optimal portfolio configuration (Pandas DataFrame).
+
+Parameters:<br>
+* API_KEY (optional) = (str) API Key from Market Stack (only requried if using this method to build portfolio).
+* verbose (optional, default = False) = (bool) Turn on if you like Zelda jokes.
+
+### Updating Parameters
+Set API Key:<br>
+`portfolio_object.set_API_KEY(<STR>)`<br><br>
+
+Set verbose:<br>
+`portfolio_object.set_verbose(<BOOL>)`<br>
+
+### Building Portfolio
+Portfolio objects can be instantiated by uploadng a CSV of portfolio performance, or using the Market Stack API (https://marketstack.com/, API key required - note that access may be limited if using an unpaid account).<br><br>
+
+Market Stack API:<br>
+`portfolio_object.build_portfolio(TKR_list, time_delta, **options)`<br>
+
+Parameters:<br>
+* TKR_list (required) = (list) List of ticker symbols for portfolio.
+* time_delta (required) = (int) Number of days to collect price data (either from today or from end_date).
+* end_date (optional, default = today's date) = (str, %m-%d-%Y) Specify the end date for the time delta calculation.<br><br>
+
+Upload CSV:<br>
+`portfolio_object.import_portfolio(input_path, **options)`<br>
+
+Parameters:<br>
+* input_path (required) = (str) Location of CSV file.
+* filename (optional, default = 'portfolio.csv') = (str) Optional file name for portfolio CSV file.
+* dates_kw (optional, default = 'date') = (str) Name of column in portfolio that contains the date of each closing price.<br><br>
+
+Export Portfolio:<br>
+`portfolio_object.save(file_path, **options)`<br>
+
+Parameters:<br>
+* file_path (required) = (str) Location of CSV file.
+* filename (optional, default = 'portfolio.csv') = (str) Optional file name for portfolio CSV file.
+
+### Finding Optimal Weights
+Implements the Markowitz CLA algorithm.<br><br>
+
+`portfolio_object.optimize(**options)`<br>
+
+Parameters:<br>
+* mu (optional, default = None) = (float) When not None, algorithm will return the Sharpe ratio portfolio; otherwise will return the NCO portfolio.
+* maxNumClusters (optional, default = 10 or number of stocks in portfolio - 1) = (int) Maximum number of clusters.  Must not exceed the number of stocks in the portfolio - 1.
+
+
+### Trend Analysis
+Trend analysis can be performed on securities within the portfolio.  Output is a Pandas DataFrame.<br><br>
+
+`trend_output = portfolio_object.trend(**options)`<br>
+
+Parameters:<br>
+* exclude (optional, default = []) = (list) List of ticker symbols in portfolio to exclude from trend analysis.  Default setting will include all items in portfolio.
+'''
     print(helptext)
 
 
